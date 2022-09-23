686387ffa6ba0ba4daf3e92d9e036ce26088d294
Pierre Ribault <pribault@MBP-de-Pierre.lan>
2022-09-23 16:12:23 +0200

    Add Players

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,40 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (div, text)
 
 
 main =
-    text "It's time for tik-tak-toe"
+    div []
+        [ text (displayPlayer Nothing)
+        , text (displayPlayer (Just Player1))
+        , text (displayPlayer (Just (switchPlayer Player1)))
+        ]
+
+
+type Player
+    = Player1
+    | Player2
+
+
+displayPlayer : Maybe Player -> String
+displayPlayer player =
+    case player of
+        Nothing ->
+            "?"
+
+        Just p ->
+            case p of
+                Player1 ->
+                    "O"
+
+                Player2 ->
+                    "X"
+
+
+switchPlayer : Player -> Player
+switchPlayer player =
+    if player == Player1 then
+        Player2
+
+    else
+        Player1
